df30b0984ad1af2576619cf2cb4a184aa19dd3e2
Junith-K <junithjunith28@gmail.com>
2023-09-28 11:56:28 +0530

    sound done

--- components/SoundBar.js
+++ components/SoundBar.js
@@ -1,49 +1,5 @@
 import React, { useRef, useState } from 'react'
-import styled, { keyframes } from 'styled-components'
-
-const Box = styled.div`
-display:flex;
-cursor:pointer;
-z-index:10;
-
-&>*:nth-child(1){
-    animation-delay: 0.2s;
-}
-&>*:nth-child(2){
-    animation-delay: 0.3s;
-}
-&>*:nth-child(3){
-    animation-delay: 0.4s;
-}
-&>*:nth-child(4){
-    animation-delay: 0.5s;
-}
-&>*:nth-child(5){
-    animation-delay: 0.8s;
-}
-`
-
-const play = keyframes`
-0%{
-    transform:scaleY(1);
-}
-50%{
-    transform:scaleY(2);
-}
-100%{
-    transform:scaleY(1);
-}
-` 
-const Line = styled.span`
-background-color: white;
-border: 1px solid black;
-
-animation:${play} 1s ease infinite;
-animation-play-state: ${props => props.click ? "running" : "paused"};
-height: 1rem;
-width: 4px;
-margin:0 0.1rem
-`
+import style from './styles/soundbar.module.css'
 
 const SoundBar = () => {
 
@@ -60,17 +16,19 @@ const SoundBar = () => {
         }
     }
     return (
-        <Box onClick={() => handleClick()}>
-            <Line click={click}/>
-            <Line click={click}/>
-            <Line click={click}/>
-            <Line click={click}/> 
-            <Line click={click}/>
+        <div className={style.Box} onClick={() => handleClick()}>
+            <span style={{animationPlayState: click?"running": "paused"}} className={style.liness} click={click}/>
+            <span style={{animationPlayState: click?"running": "paused"}} className={style.liness} click={click}/>
+            <span style={{animationPlayState: click?"running": "paused"}} className={style.liness} click={click}/>
+            <span style={{animationPlayState: click?"running": "paused"}} className={style.liness} click={click}/> 
+            <span style={{animationPlayState: click?"running": "paused"}} className={style.liness} click={click}/>
   
  
             <audio src='/song2.mp3' ref={ref} loop />
-        </Box>
+        </div>
     )
 }
 
 export default SoundBar
+
+// animation-play-state: ${props => props.click ? "running" : "paused"};
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import Avatar2 from "../components/Avatar2";
 const Home = () => {
   return (
   <div className="bg-primary/60 h-full">
+    <ParticlesContainer />
     <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black-10">
       <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
         <motion.h1 
@@ -42,9 +43,6 @@ const Home = () => {
     <div className="w-[1200px] h-full absolute right-0 bottom-0">
       <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right 
       xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
-        </div>
-        <div>
-        <ParticlesContainer />
         </div>
         <motion.div 
         variants={fadeIn("up",0.5)}
